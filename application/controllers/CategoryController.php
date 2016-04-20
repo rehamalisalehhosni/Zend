efe344f0f708dd6b8c3776d820fcca761453ef03
Bassem Ghoniem <bassem.ghoniem@yahoo.com>
2016-04-20 16:02:42 +0200

    CRUD of Cat.

--- application/controllers/CategoryController.php
+++ application/controllers/CategoryController.php
@@ -11,15 +11,77 @@ class CategoryController extends Zend_Controller_Action
     public function indexAction()
     {
         $mapper = new Application_Model_CategoryMapper();
-        $this->view->categorys = $mapper->fetchAll();
+        $this->view->cats = $mapper->fetchAll();
     }
 
     public function createAction()
     {
-        // action body
+        $form = new Application_Form_CreateCategory();
+        $mapper = new Application_Model_CategoryMapper();
+        $mainCats = $mapper->findMainCats();
+        $parents = array();
+        foreach ($mainCats as $cat) {
+            $parents[$cat->getCategory_id()] = $cat->getCategory_name();
+        }
+    //    var_dump($mainCats); exit();
+        $form->setParents($parents);
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $data = $this->getRequest()->getParams();
+                $category = new Application_Model_Category($data);
+                if ($mapper->categoryExists($data['category_name'])) {
+                    return $this->view->error = 'category already exists';
+                } else {
+                    //var_dump($category); exit();
+                    $mapper->save($category);
+
+                    return $this->_helper->redirector('index');
+                }
+            }
+        }
+        $this->view->form = $form;
+
+    }
+
+    public function deleteAction()
+    {
+        $users = new Application_Model_Category();
+        $mapper = new Application_Model_CategoryMapper();
+        $id = $this->getRequest()->getParam('id');
+        $mapper->remove($id);
+        return $this->_helper->redirector('index');
+    }
+
+    public function editAction()
+    {
+        $request = $this->getRequest();
+        $id = $this->getRequest()->getParam('id');
+        $form = new Application_Form_CreateCategory();
+        $mapper = new Application_Model_CategoryMapper();
+        $cat = $mapper->find_array($id);
+        $mainCats = $mapper->findMainCats();
+        $parents = array();
+        $form->populate($cat);
+
+        foreach ($mainCats as $cat) {
+            $parents[$cat->getCategory_id()] = $cat->getCategory_name();
+        }
+    //    var_dump($mainCats); exit();
+        $form->setParents($parents);
+        #var_dump($cat); exit();
+        if ($request->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $data = $this->getRequest()->getParams();
+                $category = new Application_Model_Category($data);
+                $category->setCategory_id($id);
+                #var_dump($category); exit();
+                $mapper->save($category);
+                return $this->_helper->redirector('index');
+            }
+        }
+        $this->view->form = $form;
     }
 
 
 }
-
-
--- application/forms/CreateCategory.php
+++ application/forms/CreateCategory.php
@@ -2,36 +2,58 @@
 
 class Application_Form_CreateCategory extends Zend_Form
 {
+    private $parent;
+
+    public function init()
+    {
+        $this->parent = new Zend_Form_Element_Select('category_parent');
         $categoryName = new Zend_Form_Element_Text('category_name');
         $categoryName->class = 'formtext';
         $categoryName->setLabel('Category Name:')
                  ->setDecorators(array(
                      array('ViewHelper',
-                           array('helper' => 'formText')),
+                           array('helper' => 'formText'), ),
                      array('Label',
-                           array('class' => 'label'))
+                           array('class' => 'label'), ),
                  ));
+        $description = new Zend_Form_Element_Textarea('category_description');
+        $description->setLabel('Description :')
+            ->setRequired(true)
+            ->setAttrib('cols', '40')
+            ->setAttrib('rows', '4');
 
-
+        $this->parent->setLabel("Parent");
+        $state = new Zend_Form_Element_Radio('category_state');
+        $state->addMultiOptions(array('1'=>'on', '0'=>'off'))
+                ->setRequired(true)
+                ->setLabel("State");
         $submit = new Zend_Form_Element_Submit('Create');
         $submit->class = 'formsubmit';
         $submit->setValue('Login')
                ->setDecorators(array(
                    array('ViewHelper',
-                   array('helper' => 'formSubmit'))
+                   array('helper' => 'formSubmit'), ),
                ));
+
 
         $this->addElements(array(
             $categoryName,
-            $submit
+            $this->parent,
+            $description,
+            $state,
+            $submit,
         ));
 
         $this->setDecorators(array(
             'FormElements',
             'Fieldset',
-            'Form'
+            'Form',
         ));
     }
+    function setParents(array $parents) {
+        //var_dump($parents); exit();
 
-
+        $this->parent->addMultiOptions($parents);
+        #$this->populate(array("General" => "1"));
+    }
 }
